3cfe85d6496c5cf57068071e6c707225b48ec0aa
Jack Pierce Nelson <Jackpiercenelson@outlook.commailto:Jackpiercenelson@outlook.com>
2024-12-07 17:20:57 -0600

    Updated main.jsx and vite.config files

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/*
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,4 +8,18 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+  */
+
+// Imports necessary dependencies: React, ReactDOM, the main App component, and any global styles.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// Creates a root for your React application using ReactDOM.createRoot()
+// Renders the App component inside React's StrictMode for additional checks and warnings during development.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
